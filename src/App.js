0610ef9ff293e13dc8ea7d00726230636ce860e7
dashka0212 <dashka_0248@yahoo.com>
2019-11-02 17:58:53 +0800

    todo list
    jsonplaceholder.typicode.com

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import less from 'less';
 import {NavLink} from 'react-router-dom';
 import {testRed , testReq} from '../redux/actions';
 import {connect} from 'react-redux';
 class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        users : [],
+        todos : []
+      }
+  }
+  componentDidMount(){
+    /*
+      
+      sort();
+    */
+    //https://jsonplaceholder.typicode.com/todos?userId=1
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=1').
+          then(res => res.json()).
+          then((data) => 
+            {
+              var filt = data.sort((a , b) => { 
+                return  b.title - a.title;
+              });
+              console.log(filt);
+              this.setState({todos : filt});
+            }
+          ).catch();
+  }
   test(event){
     this.props.dispatch(testRed(event.target.value));
   }
-  testing(){
-    this.props.dispatch(testReq());
+  testing(id){
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=' + id).
+          then(res => res.json()).
+          then((data) => 
+            {
+              this.setState({todos : data})
+            }
+          ).catch();
   }
   render(){
-    console.log(this.props);
-    less.modifyVars({
-      '@defaultColor': 'blue'
-    });
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           <input onChange={this.test.bind(this)}/>
-          <p className="peasedaa" onClick={this.testing.bind(this)}>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          
+          {
+            this.state.users.map((user) => 
+              <p onClick={this.testing.bind(this , user.id)}>
+                {user.name}
+              </p>
+            )
+          }
           <NavLink to={"/about"} className="App-link">About</NavLink>
         </header>
       </div>
